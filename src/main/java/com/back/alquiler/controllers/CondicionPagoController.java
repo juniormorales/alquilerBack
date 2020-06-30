657b709959432723e24301c17db5b4055d8ce627
juniormorales <angel_96vir@hotmail.com>
2020-06-30 18:32:10 -0500

    rutas registrar modificar condicion pago

--- src/main/java/com/back/alquiler/controllers/CondicionPagoController.java
+++ src/main/java/com/back/alquiler/controllers/CondicionPagoController.java
@@ -1,9 +1,14 @@
 package com.back.alquiler.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.back.alquiler.models.CondicionPago;
 import com.back.alquiler.service.CondicionPagoService;
 
 @RestController
@@ -12,5 +17,15 @@ public class CondicionPagoController {
 	
 	@Autowired
 	CondicionPagoService service_condicion_pago;
+	
+	@PostMapping("/registrar")
+	public ResponseEntity<?> registrarCondicion(@RequestBody CondicionPago condicionPago){
+		return null;
+	}
+	
+	@PutMapping("/modificar")
+	public ResponseEntity<?> modificarCondicion(@RequestBody CondicionPago condicionPago){
+		return null;
+	}
 	
 }

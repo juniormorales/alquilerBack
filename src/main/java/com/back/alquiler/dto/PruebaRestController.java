ebe590c16112f8e7a1d2f0ac24f8a97e97eb1e98
juniormorales <angel_96vir@hotmail.com>
2020-08-13 00:55:32 -0500

    test

--- src/main/java/com/back/alquiler/dto/PruebaRestController.java
+++ src/main/java/com/back/alquiler/dto/PruebaRestController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,11 +32,39 @@ public class PruebaRestController {
 		Map<String,Object> response = new HashMap<>();
 		dato.setId_usuario(1);
 		dato.setToken("Asadh6ju435S%dfgbrWSqAwEQdRaeR#");
+		Dato1 persona = new Dato1();
+		persona.setApellidos("Morales Brenis");
+		persona.setNombres("Junior Angel");
+		persona.setEdad(23);
+		persona.setEmail("angel_96vir@hotmail.com");
+		persona.setDni("73088001");
+		persona.setGenero("Masculino");
+		persona.setId_persona(1);
+		persona.setUsuario(dato);
+
 		System.out.println(dato.toString());
 		response.put("isSuccesfull", true);
 		response.put("message","Usuario registrado correctamente");
-		response.put("usuario",dato);
+		response.put("persona",persona);
 		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.OK);
 		
+	}
+	
+	@GetMapping("/obtenerPersona/{id}")
+	public ResponseEntity<?> obtenerPersona(@PathVariable("id") Integer id){
+		System.out.println("ID del usuario: " + id);
+		Dato1 persona = new Dato1();
+		persona.setApellidos("Morales Brenis");
+		persona.setNombres("Junior Angel");
+		persona.setEdad(23);
+		persona.setGenero("Masculino");
+		persona.setId_persona(1);
+		persona.setEmail("angel_96vir@hotmail.com");
+		persona.setDni("73058001");
+		Map<String,Object> response = new HashMap<>();
+		response.put("isSuccesfull", true);
+		response.put("message","Listado correctamente");
+		response.put("objeto",persona);
+		return new ResponseEntity<Map<String,Object>>(response,HttpStatus.OK);
 	}
 }
